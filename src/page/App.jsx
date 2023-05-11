682182d0195a46b0bf2cf9b972e2b7f16af02df5
FinoanaNyAina <finoanaandriantsoa@gmail.com>
2023-05-11 18:44:44 +0300

    premier basculement

--- src/page/App.jsx
+++ src/page/App.jsx
@@ -1,66 +1,11 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import Home from './home/components/Home';
-import Voiture from './auto/components/Voiture';
-import Collaborateur from './collaborateur/components/Collaborateur';
-import './app.css';
-import Car from '../images/Car.png';
-import Fiche from '../images/Configuration.png';
-import User from '../images/user.png';
-import Liste from '../images/clipboard.png';
+//import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
     return (
 
         <>
-            <div className='body'>
-                <div className='title'>
-                    <div>
-                        <p className='logoP' >GESTION Voiture</p>
-                    </div>
-                    <div>
-                        <p>About</p>
-                    </div>
-                </div>
-
-                <div className='container'>
-                    <div className='nav-gauche'>
-                        <div>
-                            <nav className='nav'>
-                                <ul className='ul-nav'>
-                                    <li className='li-nav1'><Link to="/voiture" className='link'><img src={Liste} className='logo' alt='logo cehicule' /></Link></li>
-                                </ul>
-                                <ul className='ul-nav'>
-                                    <li className='li-nav2'><Link to="/collabo" className='link'> <img src={User} className='logo' alt='logo cehicule' /></Link></li>
-                                </ul>
-                                <ul className='ul-nav'>
-                                    <li className='li-nav3'><Link to="/" className='link'> <img src={Fiche} className='logo' alt='logo cehicule' /></Link></li>
-                                </ul>
-                                <ul className='ul-nav'>
-                                    <li className='li-nav4'><Link to="/" className='link'> <img src={Car} className='logo' alt='logo cehicule' /></Link></li>
-                                </ul>
-
-
-                            </nav>
-                        </div>
-
-                    </div>
-
-                    <div className='nav-droite'>
-
-                        <Routes>
-                            <Route path='/' element={<Home />} ></Route>
-                            <Route path='/voiture' element={<Voiture />}></Route>
-                            <Route path='/collabo' element={<Collaborateur />}></Route>
-                        </Routes>
-
-                    </div>
-
-                </div>
-            </div>
-
-
 
         </>
     )
